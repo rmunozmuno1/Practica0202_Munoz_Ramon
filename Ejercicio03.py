6f5e7d7222ec6488d3178599b1e2abd992d44455
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-10-14 12:04:01 +0200

    Ejercicio 2 y 3

---
+++ Ejercicio03.py
@@ -0,0 +1,3 @@
+### Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla <NOMBRE> tiene <n> letras, donde <NOMBRE> es el nombre de usuario en mayúsculas y <n> es el número de letras que tienen el nombre.
+Nombre_del_usuario= input('¿Cual es tu nombre?').upper()
+print (Nombre_del_usuario, 'tiene,', len(Nombre_del_usuario), 'letras')

49f0b27ee13491608bd1666674dcd745c6192eeb
rmunozmuno1 <rmunozmuno1@educacion.navarra.es>
2024-10-18 20:14:53 +0200

    Ejercicio05

---
+++ Ejercicio05.py
@@ -0,0 +1,4 @@
+###Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida.
+frase= input('Introduzca una frase')
+Frase_al_reves = frase[::-1]
+print (Frase_al_reves)
